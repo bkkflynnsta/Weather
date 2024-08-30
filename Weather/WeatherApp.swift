46113a128863afd82a16c01fa55246fc191ea2d6
Jeff Flynn <96162611+mula-jeff@users.noreply.github.com>
2024-08-30 10:35:34 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jeffrey Michael Flynn on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
